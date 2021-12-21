d6d948d26ebd4023a211a7e0246903a1ac61c039
suuhm <blackblocks@web.de>
2021-12-21 22:32:25 +0100

    Update python_upgrader.sh

--- python_upgrader.sh
+++ python_upgrader.sh
@@ -5,13 +5,15 @@ echo "Install/Upgrade Python $VERS Helper - BY suuhmer (C) 2021"
 echo "Mainly tested and used on Debian/Ubuntu Linux OS"
 echo
 
-if [ -z $VERS ]
+if [ -z $VERS ]; then
   echo "Usage $0 VERSION (See full list with: $0 --list-versions)"
+  exit 1
 fi
 
 if [ "$1" == "--list-versions"]; then
   echo -e "List of actual python versions available:\n"
   curl -sL https://www.python.org/downloads/ | grep -E "Python\ [1-3]\.[0-9].[0.9]" | sed -r 's/.*\ ([0-9].*[0-9]).*/\1/g'
+  exit 0
 fi
 
 apt update && apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev \
@@ -24,7 +26,7 @@ echo
 wget https://www.python.org/ftp/python/$VERS/Python-$VERS.tgz
 tar xvf Python-$VERS.tgz
 cd Python-$VERS
-./configure --enable-optimizations --enable-shared
+./configure --enable-optimizations --enable-shared --with-ensurepip=install
 make -j8 && make altinstall
 
 echo && sleep 2
